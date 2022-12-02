22034bb78256e0ce7905358ce738e1ce83f62fe3
AliceShi25 <shialice2006@gmail.com>
2022-12-02 13:17:11 -0500

    t1t2

--- vite-project/js/index.js
+++ vite-project/js/index.js
@@ -1,3 +1,7 @@
 import "../styles/style.css";
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
+// ..
+AOS.init();
 import { stuff } from "./menu";
 console.log(stuff);
