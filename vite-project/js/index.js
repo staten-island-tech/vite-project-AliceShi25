175cded74f02bd239f7cab716e37b2699c66b28f
AliceShi25 <shialice2006@gmail.com>
2022-12-05 14:37:09 -0500

    box

--- vite-project/js/index.js
+++ vite-project/js/index.js
@@ -3,8 +3,8 @@ import AOS from "aos";
 import "aos/dist/aos.css"; // You can also use <link> for styles
 // ..
 AOS.init();
-import { stuff } from "./menu";
-console.log(stuff);
+import { stuff } from "./stuff";
+// console.log(stuff);
 
 document.querySelector("#theme").addEventListener("click", function () {
   if (document.body.classList.contains("pinkTheme")) {
@@ -15,3 +15,17 @@ document.querySelector("#theme").addEventListener("click", function () {
     document.body.classList.remove("defaultTheme");
   }
 });
+
+const createBoxes = function () {
+  stuff.forEach((stuff) =>
+    document.querySelector(".parent").insertAdjacentHTML(
+      "beforeend",
+      `<div class="box">
+        <h3>${stuff.name}</h3>
+        <img class="image" src="${stuff.image}">
+        <p class="info">${stuff.price}</p>
+        </div>`
+    )
+  );
+};
+createBoxes();
