92d7a4ee12a30ccd393202535d4341951aacd046
AliceShi25 <shialice2006@gmail.com>
2022-12-07 13:14:23 -0500

    f

--- vite-project/js/stuff.js
+++ vite-project/js/stuff.js
@@ -78,6 +78,102 @@ const stuff = [
     popular: false,
     inStock: true,
   },
+  {
+    name: "Aunt Annie",
+    price: "$62.00",
+    image:
+      "https://www.seussville.com/app/uploads/2019/11/character-Aunt-Annie.png",
+    cheap: true,
+    popular: true,
+    inStock: true,
+  },
+  {
+    name: "Barthlomew Cubbins",
+    price: "$1.00",
+    image:
+      "https://www.seussville.com/app/uploads/2019/11/character-Bartholomew-Cubbins.png",
+    cheap: true,
+    popular: false,
+    inStock: false,
+  },
+  {
+    name: "Fish",
+    price: "$1.00",
+    image:
+      "https://www.seussville.com/app/uploads/2019/11/character-Fish-Cat-in-the-Hat.png",
+    cheap: true,
+    popular: false,
+    inStock: true,
+  },
+  {
+    name: "Blogg",
+    price: "$0.50",
+    image: "https://www.seussville.com/app/uploads/2019/11/character-Blogg.png",
+    cheap: true,
+    popular: false,
+    inStock: true,
+  },
+  {
+    name: "Bolster",
+    price: "$75.00",
+    image:
+      "https://www.seussville.com/app/uploads/2019/11/character-Bolster.png",
+    cheap: false,
+    popular: false,
+    inStock: false,
+  },
+  {
+    name: "Boy From Dr.Seuss's ABC",
+    price: "$15.00",
+    image:
+      "https://www.seussville.com/app/uploads/2019/11/character-Boy-from-ABC.png",
+    cheap: true,
+    popular: false,
+    inStock: true,
+  },
+  {
+    name: "Ghair",
+    price: "$0.75",
+    image: "https://www.seussville.com/app/uploads/2019/11/character-Ghair.png",
+    cheap: true,
+    popular: false,
+    inStock: true,
+  },
+  {
+    name: "Fritz",
+    price: "$3.00",
+    image: "https://www.seussville.com/app/uploads/2019/11/character-Fritz.png",
+    cheap: true,
+    popular: true,
+    inStock: true,
+  },
+  {
+    name: "Mayzie",
+    price: "$7.00",
+    image:
+      "https://www.seussville.com/app/uploads/2019/11/character-Mayzie.png",
+    cheap: true,
+    popular: false,
+    inStock: true,
+  },
+  {
+    name: "King Derwin",
+    price: "$850.00",
+    image:
+      "https://www.seussville.com/app/uploads/2019/11/character-King-Derwin.png",
+    cheap: false,
+    popular: false,
+    inStock: false,
+  },
+  {
+    name: "King Birtram",
+    price: "$850.00",
+    image:
+      "https://www.seussville.com/app/uploads/2019/11/character-King-Birtram.png",
+    cheap: false,
+    popular: true,
+    inStock: false,
+  },
 ];
 
 export { stuff };
