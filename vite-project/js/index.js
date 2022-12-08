172a7fd97fb4cca27b66fc4bafe9eb63ee0feadc
AliceShi25 <shialice2006@gmail.com>
2022-12-08 13:15:32 -0500

    remove

--- vite-project/js/index.js
+++ vite-project/js/index.js
@@ -6,26 +6,55 @@ AOS.init();
 import { stuff } from "./stuff";
 // console.log(stuff);
 
-document.querySelector("#theme").addEventListener("click", function () {
-  if (document.body.classList.contains("pinkTheme")) {
-    document.body.classList.add("defaultTheme");
-    document.body.classList.remove("pinkTheme");
-  } else {
-    document.body.classList.add("pinkTheme");
-    document.body.classList.remove("defaultTheme");
-  }
-});
-
-const createBoxes = function () {
-  stuff.forEach((stuff) =>
-    document.querySelector(".parent").insertAdjacentHTML(
-      "beforeend",
-      `<div class="box">
-        <h3>${stuff.name}</h3>
-        <img class="image" src="${stuff.image}">
-        <p class="info">${stuff.price}</p>
-        </div>`
-    )
-  );
+const DOMSelectors = {
+  button: document.querySelector(".button"),
+  theme: document.getElementById("theme"),
+  all: document.getElementById("allButton"),
+  cheap: document.getElementById("cheapButton"),
+  popular: document.getElementById("popularButton"),
+  inStock: document.getElementById("inStockButton"),
+  parent: document.querySelector(".parent"),
 };
-createBoxes();
+
+const display = {
+  changeTheme: DOMSelectors.theme.addEventListener("click", function () {
+    if (document.body.classList.contains("pinkTheme")) {
+      document.body.classList.add("defaultTheme");
+      document.body.classList.remove("pinkTheme");
+    } else {
+      document.body.classList.add("pinkTheme");
+      document.body.classList.remove("defaultTheme");
+    }
+  }),
+  createBoxes: function () {
+    stuff.forEach((stuff) =>
+      DOMSelectors.parent.insertAdjacentHTML(
+        "beforeend",
+        `<div class="box">
+          <h3>${stuff.name}</h3>
+          <img class="image" src="${stuff.image}">
+          <p class="info">${stuff.price}</p>
+          </div>`
+      )
+    );
+  },
+};
+display.createBoxes();
+display.changeTheme();
+
+const remove = function () {
+  document.querySelectorAll(".box").forEach((box) => box.remove());
+};
+const menu = {
+  all: DOMSelectors.all.addEventListener("click", function () {
+    remove();
+  }),
+  cheap: DOMSelectors.cheap.addEventListener("click", function () {}),
+  popular: null,
+  inStock: null,
+};
+
+menu.all();
+menu.cheap();
+menu.popular();
+menu.inStock();
